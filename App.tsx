3ed2ec36d19813ee7df3a7f0a9af40aec63e07d8
jotaapee <galhardojoaopedro@gmail.com>
2024-03-07 15:34:39 -0300

    consuming apis

--- App.tsx
+++ App.tsx
@@ -2,8 +2,19 @@ import { NativeBaseProvider, StatusBar } from 'native-base'
 
 import { THEMES } from './src/styles/themes';
 import Routes from './src/routes';
+import { useEffect } from 'react';
+import api from './src/services/api';
 
 export default function App() {
+
+  useEffect(() => {
+    async function getData() {
+      const result = await api.get('/paciente')
+      console.log(result.data)
+    }
+    getData()
+  }, [])
+  
   return (
     <NativeBaseProvider theme={THEMES}>
       <StatusBar backgroundColor={THEMES.colors.blue[800]} />
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,11 +1,56 @@
-import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base'
+import { VStack, Image, Text, Box, Link, useToast } from 'native-base'
 import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png'
 import { Title } from './components/Title';
 import { InputText } from './components/InputText';
 import { Buton } from './components/Button';
+import { useEffect, useState } from 'react';
+import { doLogin } from './services/AutenticationService';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { jwtDecode } from 'jwt-decode';
 
-export default function Login( { navigation } ) {
+
+export default function Login({ navigation }: any) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(true)
+  const toast = useToast()
+
+  useEffect( () => {
+    async function checkLogin() {
+      const token = await AsyncStorage.getItem('token')
+      if (token) {
+        navigation.replace('Tabs')
+      }
+      setLoading(false)
+    }
+    checkLogin()
+  },[])
+
+  async function login() {
+    const result = await doLogin(email, password)
+    if (result) {
+      const { token } = result
+      AsyncStorage.setItem('token', token)
+
+      const tokenDecoded = jwtDecode(token) as any
+      const pacientId = tokenDecoded.id
+      AsyncStorage.setItem('pacientId', pacientId)
+      navigation.replace('Tabs')
+    }
+    else {
+      toast.show({
+        title: 'Erro no Login',
+        description: 'O email ou senha não conferem',
+        backgroundColor: 'red.500'
+      })
+    }
+  }
+
+  if(loading) {
+    return null
+  }
+
   return (
     <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
       <Image source={Logo} alt="Logo Voll" />
@@ -17,15 +62,19 @@ export default function Login( { navigation } ) {
         <InputText
           label="Email"
           placeholder='Insira seu endereço de e-mail'
+          value={email}
+          onChangeText={setEmail}
         />
         <InputText
           label="Senha"
           placeholder='Insira sua senha'
-          secureTextEntry={true} 
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
         />
       </Box>
-      <Buton onPress={() => navigation.navigate('Tabs')}>Entrar</Buton>
-      
+      <Buton onPress={login}>Entrar</Buton>
+
       <Link href='' mt={2}>
         Esqueceu sua senha?
       </Link>
